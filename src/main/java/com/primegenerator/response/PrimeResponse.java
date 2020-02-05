a4c6569f638bee1d2a85c6bdca7d066879a87c23
Kelvinator07 <kelvinator4leo@gmail.com>
2020-02-05 04:53:45 +0100

    Prime Response

---
+++ src/main/java/com/primegenerator/response/PrimeResponse.java
@@ -0,0 +1,24 @@
+package com.primegenerator.response;
+
+public class PrimeResponse {
+
+    // 01 for successful, 00 for failure
+    private int responseCode;
+    private String responseMessage;
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
