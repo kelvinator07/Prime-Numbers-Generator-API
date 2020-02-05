51f3b4baa27344db98b90d2aa80f60a5598d169b
Kelvinator07 <kelvinator4leo@gmail.com>
2020-02-05 03:34:02 +0100

    Initial commit

---
+++ src/main/java/com/primegenerator/PrimeNumbersGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.primegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumbersGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumbersGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primegenerator/PrimeNumbersGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumbersGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
